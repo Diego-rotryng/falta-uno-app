86b770bd3bdcce6acd8490e76012802eaa2cbb24
Diego-rotryng <diego.rotryng.trad@gmail.com>
2025-06-23 00:02:12 -0300

    Update firebaseConfig.ts

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDxeWhhCeBkb5Y4Gu2a68Sry-zxX_Q62b4",
+  authDomain: "falta-unov2.firebaseapp.com",
+  projectId: "falta-unov2",
+  storageBucket: "falta-unov2.firebasestorage.app",
+  messagingSenderId: "431264435367",
+  appId: "1:431264435367:web:154aa4c2c511ce18b89f82"
 };
 
 export const app = initializeApp(firebaseConfig);
